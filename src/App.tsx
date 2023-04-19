13422688e91f73c24d314050bce61421779c958d
Mohammed <mohammed.laghzaoui@aca-it.be>
2023-04-19 13:50:08 +0200

    Removed unnecessary code from the startup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FormComponent from './components/Form';
+import FormValues from './models/formValues';
+import schema from './schemas/formSchema';
 
 function App() {
+  const closeHandler = () => {
+    console.log('FORM CLOSED!');
+  };
+  const submitHandler = (fromValues: FormValues) => {
+    console.log(
+      '🚀 ~ file: App.tsx:10 ~ submitHandler ~ fromValues:',
+      fromValues
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FormComponent
+        onClose={closeHandler}
+        onSubmit={submitHandler}
+        formSchema={schema}
+      />
     </div>
   );
 }
